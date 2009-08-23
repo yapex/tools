3f0586a1a7dbe88f8e8df67f97724933692ca3c3
YaPEX <yapex.yin@gmail.com>
2009-08-23 22:43:48 +0800

    chang growth_rate to array

--- investment/dcf.rb
+++ investment/dcf.rb
@@ -3,13 +3,15 @@
 require 'logger'
 
 @logger = Logger.new(STDOUT, 3, 1024000)
-@logger.level = Logger::INFO
+@logger.level = Logger::DEBUG
 
-def discounted_values(cash, discounted_rate, growth_rate=0, years=10)
+def discounted_values(cash, discounted_rate, growth_rate, years=10)
   result = []
+  last_year_cash = cash
   years.times do |i|
-    value = cash*((1+growth_rate)**i)
-    result << [value, value/((1+discounted_rate)**i)]
+    last_year_cash = last_year_cash * (1 + growth_rate[i])
+    
+    result << [last_year_cash, last_year_cash/((1+discounted_rate)**(i+1))]
   end
   result
 end
@@ -30,22 +32,24 @@ def dcf(init_cash, discounted_rate, v_growth_rate=0, pv_growth_rate=0.03, years=
   @logger.debug "     %3s  %3s" % ["value", "discounted"]
   
   values.each_with_index do |it, index|
-    @logger.debug "#{index}:\t%3d   %3d" % it
+    @logger.debug "#{index}:\t%3f   %3f" % it
     total_value += it[0]
     total_discounted_value += it[1]
   end
 
-  puts "total:\t %3d   %3d" % [total_value, total_discounted_value]
-  puts "pv:\t #{pv.floor}"
-  puts "dcf: %3d" % [total_discounted_value + pv]
+  puts "total:\t %3f   %3f" % [total_value, total_discounted_value]
+  puts "pv:\t #{pv}"
+  puts "dcf: %3f" % [total_discounted_value + pv]
   
   total_discounted_value + pv
 end
 
-#puts "please input init_cash, discounted_rate, growth_rate(default=0), pv_growth_rate(default=0.03), years(default=10)"
-#puts "example: 630, 0.09, 0.05, 0.03, 10"
+#puts "please input init_cash, discounted_rate, growth_rate, pv_growth_rate(default=0.03), years(default=10)"
+#puts "example: 630, 0.09, [0, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]"
 #input = gets
 #args = input.split
- 
-dcf(630, 0.09, 0.05)
+
+growth_rate = [0, 0.09, 0.09, 0.09, 0.09, 0.09, 0, 0.05, 0.05, 0.05]
+
+dcf(31.94, 0.07, growth_rate)
 
